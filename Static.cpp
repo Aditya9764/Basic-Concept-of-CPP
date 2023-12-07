7029d45ebc52301f554e301a77786b24ee6ca366
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-12-07 14:50:02 +0530

    Update Static.cpp

--- Static.cpp
+++ Static.cpp
@@ -1,6 +1,5 @@
-
 // C++ program to demonstrate
-// the use of static Static
+// the use of static 
 // variables in a Function
 #include <iostream>
 #include <string>
@@ -10,7 +9,7 @@ void demo()
 {
     // static variable
     static int count = 0;
-    cout << count << " ";
+    cout << count << " ";      // 0 1 2 3 4
  
     // value is updated and
     // will be carried to next
@@ -24,3 +23,8 @@ int main()
         demo();
         return 0 ;
 }
+//output 0 1 2 3 4 
+
+//You can see in the above program that the variable count is declared static.
+//So, its value is carried through the function calls.
+//The variable count is not getting initialized every time the function is called. 
