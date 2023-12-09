f640e45cb16b58651ba8eeb2b218806013556f84
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-12-09 23:52:37 +0530

    Update Local_Gobal.cpp

--- Local_Gobal.cpp
+++ Local_Gobal.cpp
@@ -1,1 +1,26 @@
+//  Inside a function or a block which is called local variables,
+//Anything between ‘{‘ and ‘}’ is said to inside a block.
+Local variables do not exist outside the block in which they are declared, i.e. they can not be accessed or used outside that block.
+// Outside of all functions which are called global variables.
 
+  /*
+// CPP program to illustrate  
+// usage of local variables  
+#include<iostream> 
+using namespace std; 
+  
+void func() 
+{    
+    // this variable is local to the 
+    // function func() and cannot be  
+    // accessed outside this function 
+    int age=18;     
+} 
+  
+int main() 
+{ 
+    cout<<"Age is: "<<age; 
+      
+    return 0; 
+} 
+*/
