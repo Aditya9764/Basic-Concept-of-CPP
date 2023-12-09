819083de0a029b3396c724bf3358ef8728f32e3e
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-12-09 23:59:37 +0530

    Update Local_Gobal.cpp

--- Local_Gobal.cpp
+++ Local_Gobal.cpp
@@ -32,3 +32,30 @@ int main()
 //Global Variables can be accessed from any part of the program
 //They are available through out the life time of a program.
 // They are declared at the top of the program outside all of the functions or blocks.
+
+
+// CPP program to illustrate  
+// usage of global variables  
+#include<iostream> 
+using namespace std; 
+  
+// global variable 
+int global = 5; 
+  
+// global variable accessed from 
+// within a function 
+void display() 
+{ 
+    cout<<global<<endl; 
+} 
+  
+// main function 
+int main() 
+{ 
+    display(); 
+      
+    // changing value of global 
+    // variable from main function 
+    global = 10; 
+    display(); 
+} 
