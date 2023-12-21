a3607e63960c43c19632a62d0b1ef948e2a9af88
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-12-21 15:32:02 +0530

    Update Pointer.cpp

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,2 +1,10 @@
 // Array of pointer 
 // pointer to array 
+#include<iostream.h>
+using namespace std;
+int main()
+{
+  int arr[5] = { 1, 2, 3, 4, 5 };
+  int* ptr ;        // pointer to array 
+  cout<<ptr<<endl ;
+}
